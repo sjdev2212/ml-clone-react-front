5f1a33b0afc3faeabbad2559c63c9bbb8edbf356
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-01-19 17:25:24 -0300

    add categories file and css

---
+++ src/components/Categories/Categories.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './categories.css'
+
+const Categories = () => {
+  return (
+    <section className="container-list">
+      <ul className="categories-list">
+        <li>Sports & Fitnes</li>
+        <li>Cars</li>
+        <li>Clothing</li>
+        <li>Videogames</li>
+        <li>Musical Istruments</li>
+        <li>Home appliances</li>
+        <li>Cell phones</li>
+        <li>Electronics</li>
+        <li>Pets</li>
+      </ul>
+    </section>
+  )
+}
+
+export default Categories
