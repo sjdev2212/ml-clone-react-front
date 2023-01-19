ccb08d62f31cbc4f930ceffc5b807b44ea1fe408
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-01-19 16:01:38 -0300

    initial commit setup

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './header.css'
+
+const Header = () => {
+  return (
+    <nav>
+        <ul className='nav-list'>
+            <li>categorie 1</li>
+            <li>categorie 2</li>
+            <li>categorie 3</li>
+            <li>categorie 4</li>
+            <li>categorie 5</li>
+        </ul>
+    </nav>
+  )
+}
+
+export default Header
