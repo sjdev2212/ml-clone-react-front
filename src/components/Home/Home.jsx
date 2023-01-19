1a66c1e849f4742c022c6e654ba082de48d698ca
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-01-19 16:35:11 -0300

    create home file

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='container'>
+        <h1>Welcome to ML</h1>
+        <h3>The place to buy things online!</h3>
+    </div>
   )
 }
 
