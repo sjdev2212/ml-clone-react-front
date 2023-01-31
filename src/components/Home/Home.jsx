0c1c9385312fd951e4c2f2e4b304d9adcc7a449f
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-01-31 17:46:36 -0300

    add conditional render

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -9,7 +9,10 @@ const Home = ({ logged }) => {
     <MDBContainer>.
         <h1>Welcome to ML</h1>
         <h3>The place to buy things online!</h3>
-        <h2>Status: {logged}</h2>
+        <h2>Status: {logged.loggedInStatus}</h2>
+{/*       <span>{
+        logged.loggedInStatus === 'NOT_LOGGED_IN' ? 'You are not logged in' : (<p>Welcome {logged.user.user.email}</p>)
+      }</span> */}
         <Categories/>
    </MDBContainer>
 
@@ -17,7 +20,7 @@ const Home = ({ logged }) => {
 }
 
 Home.propTypes = {
-  logged: PropTypes.string
+  logged: PropTypes.object
 }
 
 export default Home
