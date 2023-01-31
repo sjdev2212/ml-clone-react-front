ddba27c7ccb99fe4ffa35fc4bb576ee0daaca527
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-01-31 15:01:45 -0300

    log in func

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,52 +1,39 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
 const Login = ({ handleLogin }) => {
-  const [showUser, setShowUser] = useState([])
   const [form, setForm] = useState({
     email: '',
     password: '',
-    password_confirmation: '',
-    registrationErrors: ''
+    loginErrors: ''
   })
 
-  const registrationUrl = 'http://localhost:8000/registrations'
-
-  const getUser = async () => {
-    const response = await axios.get('http://localhost:8000/users')
-    try {
-      setShowUser(response.data)
-    } catch (error) {
-      alert(error.message)
-    }
-  }
+  const loginUrl = 'http://localhost:8000/sessions'
   const handleSubmit = async (e) => {
-    axios.post(registrationUrl, {
+    axios.post(loginUrl, {
       user: {
         email: form.email,
-        password: form.password,
-        password_confirmation: form.password_confirmation
+        password: form.password
+
       }
 
     }, {
       withCredentials: true
     }).then(response => {
-      console.log('registration res', response)
-      if (response.data.status === 'created') {
-        console.log('registration res', response)
+      console.log('login res', response.data)
+      if (response.data.status === 401) {
+        console.log('login res', response)
         handleLogin(response.data)
       }
     }
     ).catch(error => {
-      console.log('registration error', error)
+      console.log('login error', error)
     }
     )
     e.preventDefault()
   }
-  useEffect(() => {
-    getUser()
-  }, [])
 
   const handleChange = (e) => {
     setForm({
@@ -59,19 +46,15 @@ const Login = ({ handleLogin }) => {
       <h1>
         Login
       </h1>
-      <ul>
-        {showUser.map((user) => (
-          <li key={user.id}>
-            {user.email}
-          </li>
-        ))}
-      </ul>
+
       <form onSubmit={handleSubmit} >
         <input className='form-control' type='email' name='email' placeholder='Email' value={form.email} onChange={handleChange} required />
         <input className='form-control' type='password' name='password' placeholder='Password' value={form.password} onChange={handleChange} required />
-        <input className='form-control' type='password' name='password_confirmation' placeholder='Password confirmation' value={form.password_confirmation} onChange={handleChange} required />
-        <button className='btn btn-primary' type='submit'>Register</button>
+        <button className='btn btn-primary' type='submit'>Log in</button>
       </form>
+      <p>
+        Don&#39;t  have an account? <Link to='/register' >Register </Link>
+      </p>
 
     </div>
   )
