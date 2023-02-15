56728bbc2d77c9f43c586caf2e6708eda0cee141
Anibal Amoroso <anibaldaniel2212@gmail.com>
2023-02-15 12:10:45 -0300

    clean cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,35 +1,11 @@
-import React, { useEffect, useState } from 'react'
-import axios from 'axios'
-import ReactLoading from 'react-loading'
+import React  from 'react'
+
 
 const Cart = () => {
-  const [itemCart, setItemCart] = useState([])
-  const [loading, setLoading] = useState(true)
-  let pathname = window.location.pathname
-  pathname = pathname.replace('/cart/', '')
-  console.log(pathname)
 
-  const getItemCart = async () => {
-    const response = await axios.get(`http://localhost:8000/${pathname}`)
-    try {
-      setItemCart(response.data)
-      setLoading(false)
-    } catch (error) {
-      alert(error.message)
-    }
-  }
-  console.log(itemCart)
-
-  useEffect(() => {
-    getItemCart()
-  }, [])
   return (
     <div>
-      <h1>Cart Section</h1>
-      {loading && (<p>Loading...<ReactLoading type="spinningBubbles" color="green" /> </p>) }
-      <section className="box-group">
-        {itemCart[0].name}
-      </section>
+ 
 
     </div>
   )
